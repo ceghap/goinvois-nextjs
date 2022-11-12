0ceb98eceaaac7da74e204c8ec69ca75a377fc93
Ashraf Latif <hello@ceghap.com>
2022-11-12 15:58:25 +0000

    - remove head - use absolute path

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import { ContentArea } from '../components/common/ContentArea';
-import { Sidebar } from '../components/common/Sidebar';
-import '../styles/globals.css';
+import { ContentArea } from '@components/common/ContentArea';
+import { Sidebar } from '@components/common/Sidebar';
+import '@styles/globals.css';
 
 export default function RootLayout({
   children,
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
-import Head from 'next/head';
-
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>GoInvois</title>
-      </Head>
-
       <main>
         <h1 className="font-semibold text-2xl">Dashboard</h1>
       </main>
