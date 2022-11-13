f67c201657c0c6910f32716fd619312f8048fde0
ceghap <hello@ceghap.com>
2022-11-13 12:05:45 +0800

    add logo in app

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -3,10 +3,6 @@ import Head from 'next/head';
 export default async function Profile() {
   return (
     <div>
-      <Head>
-        <title>Profile</title>
-      </Head>
-
       <main>
         <h1 className="font-semibold text-2xl">Profile</h1>
         {/* purposely made an error to test error boundary */}
--- components/Sidebar/SidebarHeader.tsx
+++ components/Sidebar/SidebarHeader.tsx
@@ -2,23 +2,11 @@ import Link from 'next/link';
 
 export const SidebarHeader = () => {
   return (
-    <div className="flex items-center p-2 space-x-4">
-      <img
-        src="https://source.unsplash.com/100x100/?portrait"
-        alt=""
-        className="w-12 h-12 rounded-full dark:bg-gray-500"
-      />
-      <div>
-        <h2 className="text-lg font-semibold">Leroy Jenkins</h2>
-        <span className="flex items-center space-x-1">
-          <Link
-            rel="noopener noreferrer"
-            href="/dashboard/profile"
-            className="text-xs hover:underline dark:text-gray-400"
-          >
-            View profile
-          </Link>
-        </span>
+    <div className="flex items-center p-2 space-x-4 ">
+      <div className=" border-2 border-violet-400 w-32 px-4 py-2">
+        <Link href="/dashboard">
+          <img src="/logo.png" className="" />
+        </Link>
       </div>
     </div>
   );
