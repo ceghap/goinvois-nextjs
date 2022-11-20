cd6eff8a10dd777f385d7671a95600776e237023
ceghap <hello@ceghap.com>
2022-11-20 15:37:11 +0800

    minimalis logo

--- components/Sidebar/SidebarHeader.tsx
+++ components/Sidebar/SidebarHeader.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export const SidebarHeader = () => {
   return (
     <div className="flex items-center p-2 space-x-4 ">
-      <div className=" border-2 border-violet-400 w-32 px-4 py-2">
+      <div className="w-32 px-4 py-2">
         <Link href="/home">
           <img src="/logo.png" className="" />
         </Link>
