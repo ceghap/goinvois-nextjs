5f40f1efaeaa6486199bc8d8b1786cbff0af6cda
ceghap <hello@ceghap.com>
2022-11-21 09:41:12 +0800

    include email provider

--- next.config.js
+++ next.config.js
@@ -11,6 +11,8 @@ const nextConfig = {
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET || '',
     AUTH0_ISSUER: process.env.AUTH0_ISSUER || '',
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || '',
+    EMAIL_SERVER: process.env.EMAIL_SERVER || '',
+    EMAIL_FROM: process.env.EMAIL_FROM || '',
   },
   images: {
     remotePatterns: [
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import Auth0Provider from 'next-auth/providers/auth0';
+import EmailProvider from 'next-auth/providers/email';
 import prisma from '../../../lib/prismadb';
 
 export const authOptions = {
@@ -11,6 +12,10 @@ export const authOptions = {
       clientId: process.env.AUTH0_CLIENT_ID || '',
       clientSecret: process.env.AUTH0_CLIENT_SECRET || '',
       issuer: process.env.AUTH0_ISSUER,
+    }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
     }),
   ],
 };
