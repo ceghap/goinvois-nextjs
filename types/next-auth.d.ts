5b384c5cd27e4611dd74974febed4f4440115ada
ceghap <hello@ceghap.com>
2022-11-28 21:47:33 +0800

    fix added userid type

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,18 +1,18 @@
 import { Session } from 'next-auth';
-import { JWT } from 'next-auth/jwt';
+// import { JWT } from 'next-auth/jwt';
 
-/** Example on how to extend the built-in session types */
 declare module 'next-auth' {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
   interface Session {
-    /** This is an example. You can find me in types/next-auth.d.ts */
-    id: string;
+    user: { id: string };
   }
 }
 
-/** Example on how to extend the built-in types for JWT */
-declare module 'next-auth/jwt' {
-  interface JWT {
-    /** This is an example. You can find me in types/next-auth.d.ts */
-    bar: number;
-  }
-}
+// declare module 'next-auth/jwt' {
+//   interface JWT {
+//     /** This is an example. You can find me in types/next-auth.d.ts */
+//     bar: number;
+//   }
+// }
