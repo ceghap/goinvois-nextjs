ade22c29f7b6fd7f0bdd6cc946658b9c3069b22f
ceghap <hello@ceghap.com>
2022-12-04 20:03:41 +0800

    fix react query

--- pages/companies/index.tsx
+++ pages/companies/index.tsx
@@ -2,10 +2,31 @@ import { CompaniesSectionHeader } from '@components/companies/CompaniesSectionHe
 import { CompaniesTable } from '@components/companies/CompaniesTable';
 import { PrivateLayout } from '@components/layouts/PrivateLayout';
 import { Company } from '@prisma/client';
+import { useQuery } from '@tanstack/react-query';
 import { GetServerSideProps } from 'next';
 import React from 'react';
 
-const Companies = ({ companies }: { companies: Company[] }) => {
+const fetchCompanies = (cookie: string = ''): Promise<Company[]> =>
+  fetch('http://localhost:3000/api/companies', {
+    method: 'GET',
+    headers: {
+      cookie,
+    },
+  }).then((response) => response.json());
+
+const Companies = ({
+  initialCompanies,
+  cookie,
+}: {
+  initialCompanies: Company[];
+  cookie: string;
+}) => {
+  const { data: companies } = useQuery({
+    queryKey: ['companies'],
+    queryFn: () => fetchCompanies(cookie),
+    initialData: initialCompanies,
+  });
+
   return (
     <>
       <CompaniesSectionHeader title="Companies" />
@@ -17,17 +38,12 @@ const Companies = ({ companies }: { companies: Company[] }) => {
 export default Companies;
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-  const res = await fetch('http://localhost:3000/api/companies', {
-    method: 'GET',
-    headers: {
-      cookie: req.headers.cookie || '',
-    },
-  });
-  const companies = await res.json();
+  const companies = await fetchCompanies(req.headers.cookie || '');
 
   return {
     props: {
-      companies,
+      initialCompanies: companies,
+      cookie: req.headers.cookie || '',
     },
   };
 };
