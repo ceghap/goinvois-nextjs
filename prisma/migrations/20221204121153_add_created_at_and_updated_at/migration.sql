c0303851bfbd1a9476c6c64599112047a430723d
ceghap <hello@ceghap.com>
2022-12-04 20:15:06 +0800

    add createdAt & updatedAt field

---
+++ prisma/migrations/20221204121153_add_created_at_and_updated_at/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `Company` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Customer` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Profile` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Company" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Customer" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Profile" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
