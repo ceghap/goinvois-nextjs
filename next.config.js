85d3908207a483183a285140ccd418eba5179d93
ceghap <hello@ceghap.com>
2022-12-05 09:40:48 +0800

    add googleusercontent path for image

--- next.config.js
+++ next.config.js
@@ -23,6 +23,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 's.gravatar.com',
       },
+      {
+        protocol: 'https',
+        hostname: '*.googleusercontent.com',
+      },
     ],
   },
 };
